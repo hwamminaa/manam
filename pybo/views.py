8c317d6f82988c01ace298e55ca9ac5a1a6d114e
Mina Hwang <hmina2001@naver.com>
2022-07-28 17:34:01 +0900

    파이보 프로젝트 최초 커밋

---
+++ pybo/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Question
+
+# Create your views here.
+def index(request):
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
+
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('pybo:detail', question_id=question.id)
