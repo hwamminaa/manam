dc01e71fdaed38464f159e0b4bbf2df04c55982e
황민아 <Email address>
2022-08-02 01:16:26 +0900

    3-05 로그인과 로그아웃까지

--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from pybo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')),
+    path('common/', include('common.urls')),
+    path('', views.index, name='index'),  # '/' 에 해당되는 path
 ]
