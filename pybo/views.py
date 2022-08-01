cb128bbab9f8d8cfbb4de042d7c34a7348812945
황민아 <Email address>
2022-08-02 00:38:28 +0900

    Jump to Django 3-04까지

--- pybo/views.py
+++ pybo/views.py
@@ -3,11 +3,15 @@ from django.utils import timezone
 from .models import Question
 from django.http import HttpResponseNotAllowed
 from .forms import QuestionForm, AnswerForm
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
+    page = request.GET.get('page', '1')  # 페이지
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list}
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+    context = {'question_list': page_obj}
     return render(request, 'pybo/question_list.html', context)
 
 def detail(request, question_id):
