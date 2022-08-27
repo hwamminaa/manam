9143deccdfc657907b3a1ce363bed9f3c45c2c15
황민아 <Email address>
2022-08-27 23:52:55 +0900

    db postgresql로 변경

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,22 @@
+import environ
+
 from .base import *
 
 ALLOWED_HOSTS = ['43.200.226.59', 'manam.co.kr']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
+
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / '.env')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': '5432',
+    }
+}
