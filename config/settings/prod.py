51705e4b8247e762ee053447c986c91a39f7cd8a
황민아 <Email address>
2022-08-28 18:44:47 +0900

    최종!!!!!!!

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -5,7 +5,7 @@ from .base import *
 ALLOWED_HOSTS = ['43.200.226.59', 'manam.co.kr']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
-DEBUG = True
+DEBUG = False
 
 env = environ.Env()
 environ.Env.read_env(BASE_DIR / '.env')
